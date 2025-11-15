556878878796bdde1c70ffb9e7f0f128617ddc9b
Zodac <devblueb@gmail.com>
2025-11-15 16:44:55 -0500

    Update overlay_tweak.mm

--- overlay_tweak.mm
+++ overlay_tweak.mm
@@ -1,67 +1,42 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface OverlayViewController : UIViewController
+@interface HelloOverlay : UIView
 @end
-
-@implementation OverlayViewController {
-    UIView *overlay;
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-
-    if (overlay) return;
-
-    CGRect screen = [UIScreen mainScreen].bounds;
-
-    overlay = [[UIView alloc] initWithFrame:screen];
-    overlay.backgroundColor = [UIColor whiteColor];
-    overlay.userInteractionEnabled = YES;
-
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, screen.size.width, 100)];
-    label.text = @"Hello";
-    label.textAlignment = NSTextAlignmentCenter;
-    label.font = [UIFont boldSystemFontOfSize:40];
-    label.center = CGPointMake(screen.size.width/2, screen.size.height/2 - 40);
-
-    UIButton *okBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    okBtn.frame = CGRectMake(0, 0, 200, 50);
-    okBtn.center = CGPointMake(screen.size.width/2, screen.size.height - 120);
-    [okBtn setTitle:@"OK" forState:UIControlStateNormal];
-    okBtn.titleLabel.font = [UIFont systemFontOfSize:28 weight:UIFontWeightSemibold];
-    [okBtn addTarget:self action:@selector(hideOverlay) forControlEvents:UIControlEventTouchUpInside];
-
-    [overlay addSubview:label];
-    [overlay addSubview:okBtn];
-
-    [self.view addSubview:overlay];
-}
-
-- (void)hideOverlay {
-    [overlay removeFromSuperview];
-    overlay = nil;
-}
-
+@implementation HelloOverlay
 @end
-
 
 static void (*orig_viewDidAppear)(id, SEL, BOOL);
 
 static void replaced_viewDidAppear(id self, SEL _cmd, BOOL animated) {
     orig_viewDidAppear(self, _cmd, animated);
-
-    static dispatch_once_t once;
-    dispatch_once(&once, ^{
-        OverlayViewController *overlay = [OverlayViewController new];
-        [self presentViewController:overlay animated:NO completion:nil];
-    });
+    UIViewController *vc = (UIViewController *)self;
+    if ([vc.view viewWithTag:99999] != nil) return;
+    CGRect frame = vc.view.bounds;
+    HelloOverlay *overlay = [[HelloOverlay alloc] initWithFrame:frame];
+    overlay.backgroundColor = [UIColor whiteColor];
+    overlay.tag = 99999;
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 100)];
+    label.text = @"Hello";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont boldSystemFontOfSize:40];
+    label.center = CGPointMake(frame.size.width/2, frame.size.height/2 - 50);
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(frame.size.width/2 - 100, frame.size.height - 150, 200, 50);
+    [button setTitle:@"OK" forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:25];
+    button.backgroundColor = [UIColor lightGrayColor];
+    button.layer.cornerRadius = 10;
+    [button addTarget:overlay action:@selector(removeFromSuperview) forControlEvents:UIControlEventTouchUpInside];
+    [overlay addSubview:label];
+    [overlay addSubview:button];
+    [vc.view addSubview:overlay];
 }
 
 __attribute__((constructor))
-static void init_dylib() {
-    Class vc = NSClassFromString(@"UIViewController");
-    Method m = class_getInstanceMethod(vc, @selector(viewDidAppear:));
-    orig_viewDidAppear = (void *)method_getImplementation(m);
+static void ctor() {
+    Class cls = objc_getClass("UIViewController");
+    Method m = class_getInstanceMethod(cls, @selector(viewDidAppear:));
+    orig_viewDidAppear = reinterpret_cast<void (*)(id, SEL, BOOL)>(method_getImplementation(m));
     method_setImplementation(m, (IMP)replaced_viewDidAppear);
 }
