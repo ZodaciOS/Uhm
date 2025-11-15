d3c0c1eb58bd7343a91c8ff59aa14cdf991b6270
Zodac <devblueb@gmail.com>
2025-11-15 17:01:28 -0500

    Replace HelloOverlay with DragCircle for overlay

--- overlay_tweak.mm
+++ overlay_tweak.mm
@@ -1,9 +1,31 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface HelloOverlay : UIView
+@interface DragCircle : UIView
 @end
-@implementation HelloOverlay
+
+@implementation DragCircle {
+    CGPoint offset;
+}
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    self.backgroundColor = [UIColor redColor];
+    self.layer.cornerRadius = frame.size.width/2;
+    self.userInteractionEnabled = YES;
+    return self;
+}
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *t = [touches anyObject];
+    CGPoint p = [t locationInView:self];
+    offset = p;
+}
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *t = [touches anyObject];
+    CGPoint p = [t locationInView:self.superview];
+    self.center = CGPointMake(p.x - offset.x + self.bounds.size.width/2,
+                              p.y - offset.y + self.bounds.size.height/2);
+}
 @end
 
 static void (*orig_viewDidAppear)(id, SEL, BOOL);
@@ -11,28 +33,11 @@ static void (*orig_viewDidAppear)(id, SEL, BOOL);
 static void replaced_viewDidAppear(id self, SEL _cmd, BOOL animated) {
     orig_viewDidAppear(self, _cmd, animated);
     UIViewController *vc = (UIViewController *)self;
-    if ([vc.view viewWithTag:99999] != nil) return;
-    CGRect frame = vc.view.bounds;
-    HelloOverlay *overlay = [[HelloOverlay alloc] initWithFrame:frame];
-    overlay.backgroundColor = [UIColor whiteColor];
-    overlay.tag = 99999;
-    overlay.userInteractionEnabled = YES;
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 100)];
-    label.text = @"Hello";
-    label.textAlignment = NSTextAlignmentCenter;
-    label.font = [UIFont boldSystemFontOfSize:40];
-    label.center = CGPointMake(frame.size.width/2, frame.size.height/2 - 50);
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-    button.frame = CGRectMake(frame.size.width/2 - 100, frame.size.height - 150, 200, 50);
-    [button setTitle:@"OK" forState:UIControlStateNormal];
-    button.titleLabel.font = [UIFont boldSystemFontOfSize:25];
-    button.backgroundColor = [UIColor lightGrayColor];
-    button.layer.cornerRadius = 10;
-    [button addTarget:overlay action:@selector(removeFromSuperview) forControlEvents:UIControlEventTouchUpInside];
-    [overlay addSubview:label];
-    [overlay addSubview:button];
-    [vc.view addSubview:overlay];
-    [vc.view bringSubviewToFront:overlay];
+    if ([vc.view viewWithTag:77777] != nil) return;
+    DragCircle *circle = [[DragCircle alloc] initWithFrame:CGRectMake(100, 200, 80, 80)];
+    circle.tag = 77777;
+    [vc.view addSubview:circle];
+    [vc.view bringSubviewToFront:circle];
 }
 
 __attribute__((constructor))
